29dde383dfc0451b265688faee16906ee81c30ca
Darío <dariomadeira@gmail.com>
2023-12-20 20:16:50 -0300

    Arreglo del location

--- lib/config/geolocation/geo_location.dart
+++ lib/config/geolocation/geo_location.dart
@@ -1,11 +1,6 @@
-
 import 'dart:developer';
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-
-
 
 class Geolocation extends StatefulWidget {
   const Geolocation({super.key});
@@ -15,68 +10,45 @@ class Geolocation extends StatefulWidget {
 }
 
 class _GeolocationState extends State<Geolocation> {
-  Future<Position> determinePosition() async{
+
+  Future<Position> determinePosition() async {
     LocationPermission permission;
     permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied){
+    if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
-
-    if (permission == LocationPermission.denied){
-      return Future.error('error');
-    }
+      if (permission == LocationPermission.denied) {
+        return Future.error('error');
+      }
     }
     return await Geolocator.getCurrentPosition();
-
-
   }
 
-  void getCurrentLocation() async{
-    Position position = await determinePosition();
-     
+  String currentAdress = 'dario';
 
-  }
 
   @override
   Widget build(BuildContext context) {
-    Position? _currentLocation;
-    late bool servicePermission = false;
-    late LocationPermission permission;
-    String _currentAdress = '';
-
-
+    Position? currentLocation;
 
     return Scaffold(
-        appBar: AppBar(
-          
-      title: Text('localizacion'),
-      
-      centerTitle: true,
-          
-
-    ),
-    body: Column(
-      children: [
-        FloatingActionButton(
-          child: Text ('Tocame'),
-          onPressed: () async {
-        _currentLocation = await determinePosition();
-          inspect(_currentLocation); 
-          setState(() {
-            _currentAdress = _currentLocation!.latitude.toString();
-          });
-          }
-          
-        ),
-        
-      Text(_currentAdress)
-      ],
-      
-    ),
-      
-      
-      
-      
+      appBar: AppBar(
+        title: const Text('localizacion'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          FloatingActionButton(
+              child: const Text('Tocame'),
+              onPressed: () async {
+                currentLocation = await determinePosition();
+                inspect(currentLocation);
+                setState(() {
+                  currentAdress = currentLocation!.latitude.toString();
+                });
+              }),
+          Text(currentAdress)
+        ],
+      ),
     );
-    
   }
 }
