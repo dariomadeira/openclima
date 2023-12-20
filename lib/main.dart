8e7694b1231c728e39dbde940037e98cb412d68b
Darío <dariomadeira@gmail.com>
2023-12-20 20:00:48 -0300

    poner fuentes personalizadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:openclima/config/routers/app_routers.dart';
 import 'package:openclima/screens/home_screen.dart';
 import 'package:openclima/services/shared_preferences_service.dart';
@@ -19,6 +20,11 @@ class MyApp extends StatelessWidget {
       routes: appRoutes,
       title: 'Material App',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.sourceCodeProTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       home: const HomeScreen(),
     );
   }
